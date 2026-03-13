4ee2756ce9e38b049bab5f902e9fb3f1a723476d
janvii1709 <janhavi.singh1709@gmail.com>
2026-03-14 00:19:31 +0530

    Empty dev branch

---
+++ src/main/java/com/example/QunatityApps/QunatityAppsApplication.java
@@ -0,0 +1,13 @@
+package com.example.QunatityApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QunatityAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QunatityAppsApplication.class, args);
+	}
+
+}
